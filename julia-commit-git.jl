ec203d3c0e2d09a5a0c0393446f8ebdf8ee761e2
sonanmemon <sonanahmed8@gmail.com>
2025-09-20 12:17:07 -0700

    demo

--- julia-commit-git.jl
+++ julia-commit-git.jl
@@ -38,14 +38,24 @@ using CSV
 
 using RDatasets
  
-cars = dataset("datasets", "mtcars")
- 
 
 
 
-    
+print("Hello World")
 
 
+function demo(x)
+    while x <= 4
+        print(" staying alive! ")
+        if x == 5
+            break
+        end
+        x+=1
+    end
+end
+
+
+demo(4)
 
 
 
